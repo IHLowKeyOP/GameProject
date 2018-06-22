da58b1e8c2f9b8c9ee9bccb338911d4518aff7f8
Zeta <ihmdemon.exe@gmail.com>
2018-06-22 17:51:00 -0400

    adding button for index.html and creating constructor functions for main.js

--- main.js
+++ main.js
@@ -2,6 +2,27 @@
 $( document ).ready(function() {
   console.log( "main.js ready for action." );
 
+  var theGame = new Game()
+  var health = 300;
+  var atk = 300;
+  var namePlayer1 = prompt('What is the name of your ship');
+  var namePlayer1 = prompt('What is the name of your ship');
+
+  
+
+  function Player (namePlayer1,namePlayer2, health, atk,){
+    this.health= health;
+    this.atk = atk;
+}
+// Var player function
+  
+Player.prototype.atk = function(){
+  return this.atk
+}
+
+Player.prototype.health = function(){
+  return this.health
+}
 
 
 
@@ -29,12 +50,12 @@ $( document ).ready(function() {
 
 
 
-
-
-
-
-
-
+//DOM FUNCTIONS 
+document.getElementById('startGameButton').onclick = function(){
+  theGame = new Game();
+  
+  console.log('you clicked the button')
+}
 
 
 
