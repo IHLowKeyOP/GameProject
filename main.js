9764ec0d146484e1a01f247e61de0d6105fca36d
Zeta <ihmdemon.exe@gmail.com>
2018-06-23 20:32:03 -0400

    ship constructor rough draft

--- main.js
+++ main.js
@@ -2,29 +2,55 @@
 $( document ).ready(function() {
   console.log( "main.js ready for action." );
 
-  var theGame = new Game()
-  var health = 300;
-  var atk = 300;
-  var namePlayer1 = prompt('What is the name of your ship');
-  var namePlayer1 = prompt('What is the name of your ship');
-
-  
-
-  function Player (namePlayer1,namePlayer2, health, atk,){
-    this.health= health;
-    this.atk = atk;
-}
-// Var player function
-  
-Player.prototype.atk = function(){
-  return this.atk
-}
-
-Player.prototype.health = function(){
-  return this.health
-}
 
 
+
+// var spd = 10;
+// var health= 300;
+// var atk = 400;
+
+// //this ship is the balanced stat ship
+// function Ship(typ3,spd, health, atk,name){
+//   this.spd = spd;
+//   this.health = health;
+//   this.atk = atk;
+//   this.name = prompt('What is the name of your ship');
+//   this.typ3 = "red";
+
+// }
+
+// function glassCannon(){
+//   spd = 25;
+//   health = 50;
+//   atk = 300;
+// }
+
+// // onclick insert ship type in to new ship function to give values 
+//  function glassCannon(typ3,spd, health, atk,name){
+//       Ship.call(this, spd, health, atk, this)
+
+//       this.spd = 100;
+//       this.health = 1 ;
+//       this.atk = 59900;
+      
+//   }e
+
+// shipG1 = new glassCannon();
+
+
+// glassCannon.prototype = Object.create(Ship.prototype);
+// glassCannon.prototype.constructor = glassCannon;
+
+// console.log(shipG1)
+
+
+// // ship.prototype.atk = function(amountOfDamage){
+// //     return this.atk;
+// // }
+
+// // ship.prototype.receiveDamage= function(amountOfDamage){
+// //     this.health -= damage;
+// // }
 
 
 
