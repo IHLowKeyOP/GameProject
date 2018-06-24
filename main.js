a78b758cd9948dfc8b6b8d4a806f5402a4693e48
Giannini Charles <12gcharles@gmail.com>
2018-06-23 20:00:43 -0400

    Iterating stable build of game rules.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-// A $( document ).ready() block.
+var theGame 
+
 $( document ).ready(function() {
   console.log( "main.js ready for action." );
+
 
 
   function Game(ship1, ship2, obstacles, powerups) {
@@ -8,17 +10,25 @@ $( document ).ready(function() {
     this.ship2 = {};
     this.obstacles = [];
     this.powerups = [];
-    this.celebrateActivation = function(){
-      console.log('the game is running!');
-    };
+    // this.declareVictor HERE YOU WILL LINK METHOD for declaring the winner/loser. Reference hangman.
+    //declareVictor text below.
   }
 
  
+
+  //IN THE SHIP RECEIVEDAMAGE METHOD, INCLUDE THIS METHOD FOR THE GAME TO CHECK WHO RECEIVED DAMAGE.
+  Game.prototype.declareVictor = function(){
+    if (ship1.health ===0 && ship2.health !==0) {
+      alert(`${ship1.name} has lost, and ${ship2.name} is victorious!`);
+    } else if (ship2.health ===0 && ship1.health !==0){
+      alert(`${ship2.name} has lost, and ${ship1.name} is victorious!`);
+    } else {
+      alert(`Two ace pilots have eliminated each other in deep dark space.`);
+    }
+  }
   
 
-
-
-
+  
 
 
 
@@ -75,11 +85,11 @@ $( document ).ready(function() {
 
   document.getElementById('start-game-button').onclick = function () {
     console.log('Start Button Clicked!');
-    var theGame = new Game();
-    theGame.celebrateActivation();
+    theGame = new Game();
   }
   
-  
+
+
 
 
 
@@ -90,3 +100,102 @@ $( document ).ready(function() {
 
 
 });//END DOCUMENT READY FUNCTION
+
+
+
+
+/* THE WORKSHOP: GIANNINI'S CODE IN PROGRESS WILL GO HERE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//MOTION CONTROLS METHOD SET UP FOR SHIP
+  //Rewrite this code for the motion controls method on the ship.
+  // document.onkeydown = function (e) {
+  //   if(theGame.checkLetter(e.key)){
+  //     theGameCanvas.writeCorrectLetter(theGame.wordSoFar);
+  //   } else{
+  //     theGameCanvas.writeWrongLetter(e.key, theGame.errorsLeft);
+
+  //     theGameCanvas.drawHangMan(shapes[9 - theGame.errorsLeft]);
+  //   }
+
+
+motion controls:
+
+
+Player 1 Motion controls:
+
+Left: q
+Right:z
+Forward:s
+Back:a
+Rotate counterclockwise:w
+rotate clockwise:x
+shooting: leftcapslock
+
+Player 2 Motion controls:
+
+Left:  left arrow 
+Right: right arrow
+Forward: forward arrow
+Back: back arrow
+Rotate counterclock: /
+rotate clockwise: right shift
+shoot: .......
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
