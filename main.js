dc7fa86fa0620dddc40dfaf9e4f9aec361e78f48
Giannini Charles <12gcharles@gmail.com>
2018-06-24 21:24:42 -0400

    Incorporated event keys. Changed event.key to event.code in order to prevent ship1 cheating ship2 by using the shift button.

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ $( document ).ready(function() {
   this
   Game.prototype.declareVictor = function(){
     if (this.ship1.health ===0 && this.ship2.health !==0) {
-      alert(`${this.ship1.name} has lost, and ${ship2.name} is victorious!`);
-    } else if (ship2.health ===0 && ship1.health !==0){
-      alert(`${ship2.name} has lost, and ${ship1.name} is victorious!`);
+      alert(`${this.ship1.name} has lost, and ${this.ship2.name} is victorious!`);
+    } else if (this.ship2.health ===0 && this.ship1.health !==0){
+      alert(`${this.ship2.name} has lost, and ${this.ship1.name} is victorious!`);
     } else {
       alert(`Two ace pilots have eliminated each other in deep dark space.`);
     }
@@ -92,7 +92,15 @@ $( document ).ready(function() {
   }
   
 
-
+ //Rewrite this code for the motion controls method on the ship.
+  document.onkeydown = function (e) {
+  //  theGame.ship.motion(e.code))
+  if (event.code === 'ArrowLeft'|| event.code ==='ArrowRight'|| event.code ==='ArrowUp'|| event.code ==='ArrowDown'
+|| event.code === 'CapsLock'||event.code === 'ShiftRight'){
+    event.preventDefault();
+    console.log('no scrolling should occur here');
+    }
+  }
 
 
 
@@ -147,15 +155,7 @@ $( document ).ready(function() {
 
 
 //MOTION CONTROLS METHOD SET UP FOR SHIP
-  //Rewrite this code for the motion controls method on the ship.
-  // document.onkeydown = function (e) {
-  //   if(theGame.checkLetter(e.key)){
-  //     theGameCanvas.writeCorrectLetter(theGame.wordSoFar);
-  //   } else{
-  //     theGameCanvas.writeWrongLetter(e.key, theGame.errorsLeft);
-
-  //     theGameCanvas.drawHangMan(shapes[9 - theGame.errorsLeft]);
-  //   }
+ 
 
 
 motion controls:
