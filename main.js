ebaede492eda8c6cb595020499825f70a41c8b5f
Giannini Charles <12gcharles@gmail.com>
2018-06-26 12:19:20 -0400

    Set project on fire, redesigned code based off NickBorbe's race car

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 //Global variables
 var blah;
 var theGame;
@@ -5,13 +7,9 @@ var theImage;
 
 
 
-//Document Ready function for loading
-$(document).ready(function() {
   var ctx = document.getElementById("theCanvas").getContext("2d");
   console.log("main.js ready for action.");
   function Game(ship1, ship2, obstacles, powerups) {
-    this.ship1 = {};
-    this.ship2 = {};
     this.obstacles = [];
     this.powerups = [];
     // this.declareVictor HERE YOU WILL LINK METHOD for declaring the winner/loser. Reference hangman.
@@ -20,7 +18,7 @@ $(document).ready(function() {
 
   //Declare Victor Method. Activate via receive damage method to trigger when someone's health = 0;
   Game.prototype.declareVictor = function() {
-    if (this.ship1.health === 0 && this.ship2.health !== 0) {
+    if (this.Player1.health === 0 && this.Player2.health !== 0) {
       alert(
         `${this.ship1.name} has lost, and ${this.ship2.name} is victorious!`
       );
@@ -33,30 +31,6 @@ $(document).ready(function() {
     }
   };
 
-
-// Player 1
-  function Player1(spd, health, atk, name) {
-    this.x = 475;
-    this.y = 590;
-    this.width = 50;
-    this.height = 85;
-    this.img = "img/newShip.PNG";
-    this.spd = spd;
-    this.health = health;
-    this.atk = atk;
-    // this.name = prompt("Player 1, what will your ship be named?");
-    console.log(this);
-  }
-
-  Player1.prototype.drawShip = function() {
-    var that = this;
-    theImage = new Image();
-    theImage.src = that.img;
-    theImage.onload = function() {
-      ctx.drawImage(theImage, that.x, that.y, that.width, that.height);
-    };
-  };
-  
   
   // Player2
   function Player2(spd, health, atk, name) {
@@ -66,8 +40,8 @@ $(document).ready(function() {
     this.height = 85;
     this.img = "img/newShip.PNG";
     this.spd = 25;
-    this.health = health;
-    this.atk = atk;
+    this.health = 100;
+    this.atk = 10;
     // this.name = prompt("Player 2, what will your ship be named?");
     console.log(this);
   }
@@ -84,63 +58,61 @@ $(document).ready(function() {
 
 
   //Start Player 2 Motion Controls
-  document.onkeydown = function(event) {
-    ctx.clearRect(theP2.x, theP2.y, theP2.width, theP2.height);
-    if (event.key === "ArrowLeft") {
-      theP2.x -= theP2.spd;
-      console.log(theP2);
-    } else if (event.key === "ArrowRight") {
-      theP2.x += theP2.spd;
-      console.log("right");
-    } else if (event.key === "ArrowUp") {
-      theP2.y -= theP2.spd;
-      console.log("up");
-    } else if (event.key === "ArrowDown") {
-      theP2.y += theP2.spd;
-      console.log("down");
-    }
-    ctx.fillStyle = '#FFF';
-    ctx.fillRect(0,0,1000,700);
 
-    ctx.drawImage(theImage, theP2.x, theP2.y, theP2.width, theP2.height);
-    {
-      event.preventDefault();
-      blah = setInterval(function() {
-        Player2.prototype.controls(event.key, 1);
-      }, 10);
-    }
-  };
-  Player2.prototype.controls = function(whichCode, speed) {
+  Player2.prototype.move = function(whichKey, speed){
     ctx.clearRect(this.x, this.y, this.width, this.height);
-    switch (whichCode) {
-      case "ArrowLeft":
-        {
-          this.x -= this.spd;
-          console.log();
-        }
-        break;
-      case "ArrowRight":
-        {
-          this.x += this.spd;
-          console.log("right");
-        }
-        break;
-      case "ArrowUp":
-        {
-          this.y -= this.spd;
-          console.log("up");
-        }
-        break;
-      case "ArrowDown": {
-        this.y += this.spd;
-        console.log("down");
+  
+    switch(whichKey){
+      case 'ArrowLeft':
+      if(this.canMove(this.x - speed, this.y)){
+        this.x -=speed;
+        console.log(this);
+      }
+      break;
+      case 'ArrowRight': 
+      if(this.canMove(this.x + speed, this.y)){
+        this.x +=speed;
+        console.log(this);
+      }
+      break;
+      case 'ArrowUp':
+      if(this.canMove(this.x, this.y -speed)){
+        this.y -= speed;
+        console.log(this);
+      }
+      break;
+      case 'ArrowDown': 
+      if(this.canMove(this.x, this.y + speed)){
+        this.y +=speed;
+        console.log(this);
       }
     }
-  };
-  // End Player 2 motion controls
+    ctx.fillStyle = '#FFF'
+    ctx.fillRect(0,0,1000,700)
+    ctx.drawImage(theImage, this.x, this.y, this.width, this.height);
+    
+  }
 
 
 
+  Player2.prototype.canMove = function(futurex, futurey){
+    if(
+        futurex + this.width >= 1000 ||
+        futurex <= 0 ||
+        futurey + this.height >= 700 ||
+        futurey <= 0
+      ){
+        return false
+      }
+      return true;
+  
+  }
+  
+
+
+
+
+//End Player 2 motion controls
 
 
 
@@ -150,16 +122,28 @@ $(document).ready(function() {
   document.getElementById("start-game-button").onclick = function() {
     console.log("Start Button Clicked!");
     theGame = new Game();
-    // Must discover how the canvas works
     theGameCanvas = new GameCanvas();
     theGameCanvas.createBoard();
-    theP1 = new Player1();
     theP2 = new Player2();
-    theP1.drawShip();
-    theP2.drawShip();
+    theGame.Player2 = theP2;
+    theGame.Player2.drawShip();
   };
+
+
+  document.onkeydown = function(event) {
+    clearInterval(blah);
+    if (event.key === 'ArrowLeft'|| event.key ==='ArrowRight'|| event.key ==='ArrowUp'|| event.key ==='ArrowDown'){
+      event.preventDefault();
+
+       blah = setInterval(function(){
+        theP2.move(event.key, 2);
+      }, 10);
+    }
+  }
+
+  document.onkeyup = function(){
+    clearInterval(blah);
+  }
   
 
-  Player2.prototype.controls();
-}); //END DOCUMENT READY FUNCTION
 
