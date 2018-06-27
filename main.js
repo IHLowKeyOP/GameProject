d13406c79b41e63566864d8f8a7ed45ce3af1e4e
Giannini Charles <12gcharles@gmail.com>
2018-06-27 12:21:29 -0400

    Synthesized shooting logic into Player 2 Prototype

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//Notes: I need to figure out how to use hotkeys
 
 
+//Global Variables
 let CANVAS_WIDTH = 1000;
 let CANVAS_HEIGHT = 700;
 let canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>");
@@ -11,14 +13,22 @@ let textY = 50;
 let theP2 = new Player2();
 
 
+// GameLoop
 setInterval(function() {
   update();
   draw();
 }, 1000/FPS);
 
 
+//Game actions
 function update(){ 
+  //P2 shoot
+  if(keydown.space) {
+    theP2.shoot();
+  }
 
+
+  //P2 motion
   if(keydown.left) {
     theP2.x -= 30;
   }
@@ -34,8 +44,19 @@ if (keydown.down) {
   theP2.y += 30;
 }
 
+
+theP2.bullets.forEach(function(bullet) {
+  bullet.update();
+});
+
+theP2.bullets = theP2.bullets.filter(function(bullet) {
+  return bullet.active;
+});
+
+
 }
 
+//Motion Detection
 Player2.prototype.canMove = function(futurex, futurey){
   if(
       futurex + this.width >= 1000 ||
@@ -49,12 +70,62 @@ Player2.prototype.canMove = function(futurex, futurey){
 
 }
 
+//Shooting Logic
+Player2.prototype.shoot = function () {
+  var bulletPosition = theP2.midpoint();
+
+  theP2.bullets.push(Bullet({
+    speed: 5,
+    x: bulletPosition.x,
+    y: bulletPosition.y
+  }));
+  console.log('Pew Pew!')
+}
+
+//Bullet Logic 
+
+function Bullet(I) {
+  I.active = true;
+
+  I.xVelocity = 0;
+  I.yVelocity = -I.speed;
+  I.width = 3;
+  I.height = 6;
+  I.color = "#000";
+
+  I.inBounds = function() {
+    return I.x >= 0 && I.x <= CANVAS_WIDTH &&
+      I.y >= 0 && I.y <= CANVAS_HEIGHT;
+  };
+
+  I.draw = function() {
+    canvas.fillStyle = this.color;
+    canvas.fillRect(this.x, this.y, this.width, this.height);
+  };
+
+  I.update = function() {
+    I.x += I.xVelocity;
+    I.y += I.yVelocity;
+
+    I.active = I.active && I.inBounds();
+  };
+
+  return I;
+}
+
+
+// player.shoot = function() {
+//   
+// };
 
 
 
+
+
+//Motion protection via DOM function
 document.onkeydown = function(event) {
   
-  if (event.key === 'ArrowLeft'|| event.key ==='ArrowRight'|| event.key ==='ArrowUp'|| event.key ==='ArrowDown'){
+  if (event.key === 'ArrowLeft'|| event.key ==='ArrowRight'|| event.key ==='ArrowUp'|| event.key ==='ArrowDown'||event.key === ' '){
     event.preventDefault();}}
     
 
@@ -66,6 +137,9 @@ function draw() {
   canvas.fillStyle = "white";
   canvas.fillRect(0, 0, 1000,700);
   theP2.drawShip();
+  theP2.bullets.forEach(function(bullet) {
+    bullet.draw();
+  });
 }
 
 
@@ -80,7 +154,13 @@ function Player2(spd, health, atk, name) {
   this.health = 100;
   this.atk = 10;
   // this.name = prompt("Player 2, what will your ship be named?");
-  console.log(this);
+  this.bullets = [];
+  this.midpoint = function() {
+    return {
+      x: this.x + this.width/2,
+      y: this.y + this.height/2
+    };
+  };
 }
 
 Player2.prototype.drawShip = function() {
